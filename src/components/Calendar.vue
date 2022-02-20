5803d347537edaefd7e80172096a1dc63277cbfe
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-02-20 20:01:35 +0530

    updation and deletion done

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -61,24 +61,39 @@
         >
           <v-card color="grey lighten-4" min-width="350px" flat>
             <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                <v-icon>mdi-delete</v-icon>
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
             </v-toolbar>
             <v-card-text>
-              <span v-html="selectedEvent.details"></span>
+              <form v-if="currentlyEditing !== selectedEvent.id">
+                {{ selectedEvent.details }}
+              </form>
+              <form v-else>
+                <textarea-autosize
+                  v-model="selectedEvent.details"
+                  type="text"
+                  style="width: 100%"
+                  :min-height="100"
+                  placeholder="add note"
+                ></textarea-autosize>
+              </form>
             </v-card-text>
             <v-card-actions>
               <v-btn text color="secondary" @click="selectedOpen = false">
-                Cancel
+                Close
+              </v-btn>
+              <v-btn
+                text
+                v-if="currentlyEditing !== selectedEvent.id"
+                @click.prevent="editEvent(selectedEvent)"
+              >
+                Edit
+              </v-btn>
+              <v-btn text v-else @click="updateEvent(selectedEvent)">
+                Save
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -150,6 +165,16 @@ export default {
       });
       this.events = events;
     },
+    async updateEvent(ev) {
+      await db.collection('calEvent').doc(this.currentlyEditing).update({
+        details: ev.details,
+      });
+    },
+    async deleteEvent(ev) {
+      await db.collection('calEvent').doc(ev).delete();
+      this.selectedOpen = false;
+      this.getEvents();
+    },
     getEventColor(ev) {
       return ev.color;
     },
@@ -168,6 +193,9 @@ export default {
     },
     next() {
       this.$refs.calendar.next();
+    },
+    editEvent(ev) {
+      this.currentlyEditing = ev.id;
     },
     showEvent({ nativeEvent, event }) {
       const open = () => {
